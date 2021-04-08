19d788081e97a531e927a6b0092d9d64d8401784
OAKevin <67325994+OAKevin@users.noreply.github.com>
2021-04-08 18:09:12 -0400

    added delete and edit users for updating the database but when you click update its giving a typeError: Cannot ser property 'name' of null

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -1,11 +1,53 @@
 const User = require('../models/User');
 
-exports.getIndex = (req, res) => {
-    res.status(200).render('index');
+exports.getIndex = async (req, res) => {
+    const user = await User.find((data) => data);
+
+    try {
+        console.log(user);
+        res.status(200).render('index', { user: user });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.getUser = async (req, res) => {
+    const userId = req.params.userId;
+
+    const user = await User.findById(userId, (user) => user);
+
+    try {
+        console.log(user);
+        res.status(200).render('user', { user: user });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 exports.getAddUser = (req, res) => {
-    res.status(200).render('edit-user');
+    res.status(200).render('edit-user', { editing: false });
+};
+
+exports.getEditUser = async (req, res) => {
+    const userId = req.params.userId;
+
+    const editMode = req.query.edit;
+
+    if (!editMode) {
+        return res.redirect('/');
+    }
+
+    const user = await User.findById(userId);
+
+    try {
+        if (!userId) {
+            return res.redirect('/');
+        }
+        console.log(user);
+        res.status(200).render('edit-user', { user: user, editing: editMode });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 exports.postUser = (req, res) => {
@@ -15,4 +57,39 @@ exports.postUser = (req, res) => {
     user.save();
     console.log('User Added to the database');
     res.status(201).redirect('/');
+};
+
+exports.postEditUser = (req, res) => {
+    const userId = req.body.userId;
+    const { name, image, description } = req.body;
+
+    User.findById(userId)
+        .then((user) => {
+            user.name = name;
+            user.image = image;
+            user.description = description;
+
+            return user.save();
+        })
+        .then(() => {
+            console.log('Item Updated');
+            res.status(201).redirect('/');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+exports.postDelete = async (req, res) => {
+    const userId = req.body.userId;
+
+    const user = await User.findByIdAndRemove(userId, (data) => data);
+
+    try {
+        console.log(user);
+        console.log('Item Deleted');
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }
 };
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -7,6 +7,14 @@ router.get('/', adminController.getIndex);
 
 router.get('/add-user', adminController.getAddUser);
 
+router.get('/edit-user/:userId', adminController.getEditUser);
+
 router.post('/add-user', adminController.postUser);
+
+router.post('/edit-user', adminController.postEditUser);
+
+router.get('/:userId', adminController.getUser);
+
+router.post('/delete', adminController.postUser);
 
 module.exports = router;
